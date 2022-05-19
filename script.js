8b85824bb2d43060750d6cf94f12076ac96bb0eb
Jack Graymer <jackgraymer@gmail.com>
2022-05-19 17:34:04 +0200

    Calculator body, screen and main buttons created, basic style in place.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('satan')
+function sum(a, b){ //Sum of 2 numbers
+    return a + b;
+}
+
+function substract(a, b){//Substraction of 2 numbers
+    return a - b;
+}
+
+function multiply(a, b){//multiplication of 2 numbers
+    return a * b;
+}
+
+function divide(a, b){//Division of 2 numbers
+    return a / b;
+}
